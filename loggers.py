402574c2a8765c30b5aecf4c1709febf02037e2b
Qishen Li <manicmovier@gmail.com>
2014-12-19 12:37:30 -0500

    Remove FETCH_HEAD

--- loggers.py
+++ loggers.py
@@ -9,10 +9,7 @@ logging.config.fileConfig('logging.conf')
 logger_r = logging.getLogger('root')
 logger_c = logging.getLogger('crawler')
 logger_p = logging.getLogger('proxy')
-<<<<<<< HEAD
 
 logger_analyzer = logging.getLogger('analyzer')
 logger_db = logging.getLogger('db')
 logger_query = logging.getLogger('query')
-=======
->>>>>>> FETCH_HEAD
