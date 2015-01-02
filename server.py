63bfb7ef04107feadb2ac08eb61071dff1793bbc
Qishen Li <manicmovier@gmail.com>
2015-01-01 20:32:42 -0500

    Correct export number

--- server.py
+++ server.py
@@ -104,7 +104,7 @@ def exportResolve(fileName):
 
     # Write data from second row
     i = 0
-    for row_index in range(2, dataCounts + 1):
+    for row_index in range(2, dataCounts + 2):
         # Reduce index calculation when access a data
         data = resolve[i]
         for col_index in range(1, keySize):
