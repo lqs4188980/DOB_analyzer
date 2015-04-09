9cc1cc8a2397b7ee2c8b008c8f735f178a67dd57
Wendong-Wu <mendelwuwd@gmail.com>
2015-04-09 10:55:49 -0700

    change rotatinglog code location

--- loggers.py
+++ loggers.py
@@ -4,6 +4,25 @@ Created on Dec 16, 2014
 @author: Wendong
 '''
 import logging.config
+import os
+
+def rotateLogFile(filename, maxByte=8388608, rotate=5):
+    if os.stat(filename).st_size < maxByte:
+        return
+    rt_fils = []
+    for i in range(rotate):
+        if not os.path.isfile(filename+'.bak'+str(i)):
+            os.rename(filename, filename+'.bak'+str(i))
+            return
+        else:
+            rt_fils.append((filename+'.bak'+str(i), os.stat(filename+'.bak'+str(i)).st_mtime))
+    srt_fils = sorted(rt_fils, key=lambda x: x[1])
+    # remove oldest log
+    os.remove(srt_fils[0][0])
+    os.rename(filename, srt_fils[0][0])
+    
+if os.path.isfile('app.log'):
+        rotateLogFile('app.log')
 
 logging.config.fileConfig('logging.conf')
 logger_r = logging.getLogger('root')
